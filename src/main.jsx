047fc79251a99260ca5370e2bc080383e111fedf
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-03 18:03:03 +0100

    Fixing deps and src

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import router components
-import App from './App.jsx';  // Your main app component
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./scss/app.scss"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <Router>
-        <Routes>
-            <Route path="/ActDigitalNativeAgency/" element={<App />} />
-        </Routes>
-    </Router>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
 );
