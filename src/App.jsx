2c224b38f503bbc836e7347b199ae1cc64a14084
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-20 16:15:01 +0100

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useEffect } from "react";
-import { RouterProvider } from "react-router-dom";
+import {HashRouter, Route, RouterProvider, Routes} from "react-router-dom";
 import { router } from "./Router/Router.jsx";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Home from "./Pages/Home.jsx";
+import Root from "./Layouts/Root.jsx";
+import BlogDetails from "./Pages/BlogDetails/BlogDetails.jsx";
 
 const App = () => {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route index element={<Home />} />
+            <Route path="blog" element={<BlogDetails />} />
+          </Route>
+        </Routes>
+      </HashRouter>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  pages: {
-    index: "src/main.js",
-    other_page: "src/main.js",
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  base: '/',
 })
