ee862dd85053e57ecf65a3018e82ef7adaf596ad
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-23 14:21:39 +0100

    Added mailing

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import {useRef, useState} from "react";
 import Blog from "./Blog";
 import { blogsList } from "../../Utlits/blogList";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 
 const Blogs = () => {
   const location = useLocation();
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        const serviceId = "service_65lzo8b";
+        const templateId = "template_7xaf7ym";
+        const userId = "HNRsS6ej7z7nq8BsW";
+        emailjs
+            .sendForm(serviceId, templateId, form.current, {
+                publicKey: userId,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
 
   // Check if a specific blog route is active
   const isBlogDetailsRoute = location.pathname !== "/blog";
@@ -23,26 +44,34 @@ const Blogs = () => {
                         data-aos="fade-down"
                         data-aos-duration="1500"
                     >
-                      Nos Articles
+                      Nos articles
                     </h2>
                     <div className="blog__hoverbox">
-                      <Link
+                      <div
                           className="hover__circle wow fadeInUp"
                           data-wow-duration="1.6s"
-                          to="/"
                       >
                     <span className="box">
-                      <i className="bi bi-arrow-up-right"></i>
-                      <span className="textmore text-white"> Page d'acceuil </span>
+                      <span className="textmore text-white">
+                        Abonnez vous à notre newsletter
+                      </span>
+
+                      <form ref={form} onSubmit={sendEmail}>
+                        <div className="col-lg-12">
+                          <input className="mt-3 mb-3 text-black text-center" name="email" type="email" placeholder="Email"/>
+                        </div>
+                        <input type="submit" value="S'abonner" className="text-center cmn--btn align-items-center">
+                        </input>
+                      </form>
                     </span>
-                      </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="col-xl-8 col-lg-9">
                   <div className="blog__rightwrap">
                     <div className="service__uniquewrap">
-                      {blogsList.slice(0, 3).map(({ id, date, image, heading, linkUrl }) => (
+                      {blogsList.slice(0, 3).map(({id, date, image, heading, linkUrl}) => (
                           <Blog key={id} date={date} image={image} heading={heading} linkUrl={linkUrl} />
                       ))}
                     </div>
